f87968585adfa22171f13a9276427d660f4b110a
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-02-13 10:11:27 -0500

    Added eclipse for java

---
+++ InstallScripts/install-eclipse-java.sh
@@ -0,0 +1,3 @@
+curl http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR1a/eclipse-java-luna-SR1a-linux-gtk-x86_64.tar.gz > eclipse.tar.gz
+tar -xzf eclipse.tar.gz
+rm -rf eclipse.tar.gz
