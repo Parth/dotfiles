b7261bc562f7b6af5f411f8f4bc7c3a9cd2d453c
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-02-18 21:32:09 -0500

    Added dkms and gnome-system-tools

--- InstallScripts/ubuntu-desktop-packages.sh
+++ InstallScripts/ubuntu-desktop-packages.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
-sudo apt-get install vlc blender gimp virtualbox virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 git curl tmux vim pep8 python-dev zsh pv openjdk-7-jdk openjdk-7-jre checkinstall openssh-server python-pip build-essential gfortran libopenblas-dev liblapack-dev
+sudo apt-get install vlc blender gimp virtualbox virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 git curl tmux vim pep8 python-dev zsh pv openjdk-7-jdk openjdk-7-jre checkinstall openssh-server python-pip build-essential gfortran libopenblas-dev liblapack-dev dkms gnome-system-tools
 sudo apt-get update
