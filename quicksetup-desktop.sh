04c67fa2eb01dcdee4323baa488ccb0ef6d9c1a7
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-07-27 20:36:53 -0400

    don't need these anymore

--- quicksetup-desktop.sh
+++
@@ -1,4 +0,0 @@
-sh InstallScripts/ubuntu-desktop-packages.sh
-sh InstallScripts/install-ohmyzsh.sh
-sh InstallScripts/install-chrome.sh
-sh InstallScripts/setup-vimrc.sh
--- quicksetup-server.sh
+++
@@ -1,3 +0,0 @@
-sh InstallScripts/ubuntu-server-packages.sh
-sh InstallScripts/install-ohmyzsh.sh
-sh InstallScripts/setup-vimrc.sh
