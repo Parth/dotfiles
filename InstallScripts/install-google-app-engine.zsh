08d3e51377788604e4899467bad9cf113fba3081
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-07-27 20:31:46 -0400

    changed for new version of app engine

--- InstallScripts/install-google-app-engine.zsh
+++ InstallScripts/install-google-app-engine.zsh
@@ -4,3 +4,8 @@ curl http://sdk.cloud.google.com/ | bash
 ~/google-cloud-sdk/bin/./gcloud components update gae-python
 echo 'source "'$HOME'/google-cloud-sdk/path.zsh.inc"' >> ~/.zshrc
 echo 'source "'$HOME'/google-cloud-sdk/completion.zsh.inc"' >> ~/.zshrc
+
+cd ~
+curl -O https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.24.zip
+unzip google_appengine_1.9.24.zip
+rm -rf google_appengine_1.9.24.zip
