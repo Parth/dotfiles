ba7a2c166428a2dba5f324e8ff653e3ab66c5f48
Krishma Bawa <bawa.krishma@gmail.com>
2017-02-07 14:10:37 -0500

    newline before echo

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-echo "source '~/dotfiles/zshrc_manager.sh'" >> ~/.zshrc
-echo "so ~/dotfiles/vimrc.vim" >> ~/.vimrc
+echo "\nsource '~/dotfiles/zshrc_manager.sh'" >> ~/.zshrc
+echo "\nso ~/dotfiles/vimrc.vim" >> ~/.vimrc
