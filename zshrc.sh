eb6577a552f6d1cb009943d0ffcc07a048b9452f
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2017-04-11 16:15:07 -0400

    fixed order of opperations

--- zshrc.sh
+++ zshrc.sh
@@ -1,7 +1,3 @@
-if command -v tmux>/dev/null; then
-  [[ ! $TERM =~ screen ]] && [ -z $TMUX ] && exec tmux
-fi
-
 # Aliases
 	alias v="vim"
 
--- zshrc_manager.sh
+++ zshrc_manager.sh
@@ -1,5 +1,11 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
+# Run tmux if exists
+if command -v tmux>/dev/null; then
+  [[ ! $TERM =~ screen ]] && [ -z $TMUX ] && exec tmux
+fi
+
+
 echo "Updating rc files"
 (cd ~/dotfiles && time_out 4 git pull)
 source ~/dotfiles/zshrc.sh
