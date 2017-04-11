19475bb2a6605cf098d5413529af914818d8ede0
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2017-04-11 16:05:58 -0400

    increased timeout

--- zshrc_manager.sh
+++ zshrc_manager.sh
@@ -1,5 +1,5 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 echo "Updating rc files"
-(cd ~/dotfiles && time_out 2 git pull)
+(cd ~/dotfiles && time_out 4 git pull)
 source ~/dotfiles/zshrc.sh
