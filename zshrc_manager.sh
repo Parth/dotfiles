1ef395b11189ceb0bc83a48dca303ddb56ec667f
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2017-04-14 01:04:47 -0400

    extra space

--- zshrc_manager.sh
+++ zshrc_manager.sh
@@ -5,7 +5,6 @@ if command -v tmux>/dev/null; then
   [[ ! $TERM =~ screen ]] && [ -z $TMUX ] && exec tmux
 fi
 
-
 echo "Updating rc files"
 (cd ~/dotfiles && time_out 4 git pull)
 source ~/dotfiles/zshrc.sh
