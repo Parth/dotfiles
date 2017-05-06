1ad691d4a6f00640446c1a469339f4dbe5f2c593
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2017-05-05 22:12:25 -0400

    added timer

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -8,13 +8,26 @@ set_prompt() {
 	PS1="%{$fg[white]%}[%{$reset_color%}"
 
 	# Path
-	PS1+="%{$fg[blue]%}${PWD/#$HOME/~}%{$reset_color%}"
+	PS1+="%{$fg[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
 
 	# Status Code
 	PS1+=', '
 	PS1+='%(?.%{$fg[green]%}%?.%{$fg[red]%}%?)'
 
+	PS1+=', '
+	PS1+="%{$fg[blue]%}$_elapsed[-1]s%{$reset_color%}"
+
 	PS1+="%{$fg[white]%}]: %{$reset_color%}% "
 }
 
 precmd_functions+=set_prompt
+
+preexec () {
+   (( ${#_elapsed[@]} > 1000 )) && _elapsed=(${_elapsed[@]: -1000})
+   _start=$SECONDS
+}
+
+precmd () {
+   (( _start >= 0 )) && _elapsed+=($(( SECONDS-_start )))
+   _start=-1 
+}
