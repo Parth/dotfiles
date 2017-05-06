a677a1e1e0a2ed3d403d9ca014712ce29eb7b852
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2017-05-06 00:47:13 -0400

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
-printf "\nsource '$HOME/dotfiles/zsh/zshrc_manager.sh'" >> ~/.zshrc
-printf "\nso $HOME/dotfiles/vim/vimrc.vim" >> ~/.vimrc
-printf "\nsource-file $HOME/dotfiles/tmux/tmux.conf" >> ~/.tmux.conf
+mv ~/.zshrc ~/.zshrc.old
+mv ~/.tmux.conf ~/.tmux.conf.old
+mv ~/.vimrc ~/.vimrc.old
+
+printf "source '$HOME/dotfiles/zsh/zshrc_manager.sh'" >> ~/.zshrc
+printf "so $HOME/dotfiles/vim/vimrc.vim" >> ~/.vimrc
+printf "source-file $HOME/dotfiles/tmux/tmux.conf" >> ~/.tmux.conf
