1dc6faf225aa5915cae5c6763ea28d17e9653235
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2017-05-05 21:49:28 -0400

    precmdfunctions

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -14,16 +14,7 @@ set_prompt() {
 	PS1+=', '
 	PS1+='%(?.%{$fg[green]%}%?.%{$fg[red]%}%?)'
 
-#	# Status Code: Credit [http://stackoverflow.com/questions/16715103/bash-prompt-with-last-exit-code]
-#	PS1+=", "
-#	local EXIT="$?"
-#	if [$EXIT != 0]; then
-#	else
-#		PS1+="%{$fg[green]%} ${PWD##*/} %{$reset_color%}"
-#	fi
-#
-#	# ]
 	PS1+="%{$fg[white]%}]: %{$reset_color%}% "
 }
 
-set_prompt
+precmd_functions+=set_prompt
