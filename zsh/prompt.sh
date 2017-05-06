75a2cd5cafc007450bda3e28faa3af4c450d2831
Parth <parth.mehrotra.cs@gmail.com>
2017-05-06 00:08:14 -0400

    bolded path

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -10,7 +10,7 @@ set_prompt() {
 	PS1="%{$fg[white]%}[%{$reset_color%}"
 
 	# Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-	PS1+="%{$fg[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
+	PS1+="%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
 
 	# Status Code
 	if [[ $? -ne 0 ]]; then 
