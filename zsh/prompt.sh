bf093e67e5a38f9d5cbb3944b7ca101c6a66890e
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2017-05-05 21:44:49 -0400

    

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -3,11 +3,27 @@
  setopt PROMPT_SUBST
 
 set_prompt() {
-	# PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
-	 PS1="%{$fg[white]%}[%{$reset_color%}"
-	 PS1+="%{$fg[blue]%}${PWD##*/}%{$reset_color%}"
-	 PS1+="%{$fg[white]%}]: %{$reset_color%}"
-	
+    	local EXIT="$?"
+	# [
+	PS1="%{$fg[white]%}[%{$reset_color%}"
+
+	# Path
+	PS1+="%{$fg[blue]%}${PWD/#$HOME/~}%{$reset_color%}"
+
+	# Status Code
+	PS1+=', '
+	PS1+='%(?.%{$fg[green]%}%?.%{$fg[red]%}%?)'
+
+#	# Status Code: Credit [http://stackoverflow.com/questions/16715103/bash-prompt-with-last-exit-code]
+#	PS1+=", "
+#	local EXIT="$?"
+#	if [$EXIT != 0]; then
+#	else
+#		PS1+="%{$fg[green]%} ${PWD##*/} %{$reset_color%}"
+#	fi
+#
+#	# ]
+	PS1+="%{$fg[white]%}]: %{$reset_color%}% "
 }
 
 set_prompt
