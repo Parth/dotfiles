c85630ee8350c921f3578b50a0e4c0eddb373678
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2017-05-12 15:21:16 -0400

    update text

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -5,6 +5,6 @@ if command -v tmux>/dev/null; then
 	[[ ! $TERM =~ screen ]] && [ -z $TMUX ] && exec tmux
 fi
 
-echo "Updating rc files"
+echo "Updating configuration"
 (cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
 source ~/dotfiles/zsh/zshrc.sh
