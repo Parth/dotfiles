7592067fa069e7df8c1e27f0a5d73a67f6bfebd7
steverusso <steverusso602@gmail.com>
2017-05-17 16:08:55 +0000

    Made it so that the content added to the new files is not appended to anything there.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ mv ~/.zshrc ~/.zshrc.old
 mv ~/.tmux.conf ~/.tmux.conf.old
 mv ~/.vimrc ~/.vimrc.old
 
-printf "source '$HOME/dotfiles/zsh/zshrc_manager.sh'" >> ~/.zshrc
-printf "so $HOME/dotfiles/vim/vimrc.vim" >> ~/.vimrc
-printf "source-file $HOME/dotfiles/tmux/tmux.conf" >> ~/.tmux.conf
+printf "source '$HOME/dotfiles/zsh/zshrc_manager.sh'" > ~/.zshrc
+printf "so $HOME/dotfiles/vim/vimrc.vim" > ~/.vimrc
+printf "source-file $HOME/dotfiles/tmux/tmux.conf" > ~/.tmux.conf
+
