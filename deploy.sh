a55e85d13edb4b0dfe30fd5b8c8d79f6d21be827
steverusso <steverusso602@gmail.com>
2017-05-17 16:38:46 +0000

    Added code to check for zsh, vim, and tmux. If they are not found, the user is prompted to install them. The install command is just for FreeBSD (for now).

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,25 @@
 
+check_for_software() {
+	# command -v "$1" > /dev/null 2>&1;
+	if ! [ -x "$(command -v $1)" ]; then
+		echo -n "Heads up! $1 is not installed. Would you like to install it? (y/n) " >&2
+		old_stty_cfg=$(stty -g)
+		stty raw -echo
+		answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+		stty $old_stty_cfg && echo
+		if echo "$answer" | grep -iq "^y" ;then
+			echo "Running:	sudo pkg install $1"
+			sudo pkg install $1
+		fi
+	else
+		echo -e "$1 is installed."
+	fi
+}
+
+check_for_software zsh
+check_for_software vim
+check_for_software tmux
+
 echo -n "Would you like to backup your current dotfiles? (y/n) "
 old_stty_cfg=$(stty -g)
 stty raw -echo
