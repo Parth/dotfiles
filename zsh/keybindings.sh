04c379ef5f1d54fdf4e6173453c41293775af226
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2017-05-20 21:43:58 -0400

    sudo shortcut

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -48,9 +48,17 @@
 	zle -N ctrl_l
 	bindkey "^l" ctrl_l
 
-# Edit and rerun
+# Enter
 	function enter_line() {
 		zle accept-line
 	}
 	zle -N enter_line
 	bindkey "^o" enter_line
+
+# Sudo
+	function add_sudo() {
+		BUFFER="sudo "$BUFFER
+		zle end-of-line
+	}
+	zle -N add_sudo
+	bindkey "^s" add_sudo
