8c12b7acacf7e982fe3901542b63f58146eb8074
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2017-05-21 14:16:37 -0400

    line end

--- deploy.sh
+++ deploy.sh
@@ -36,4 +36,3 @@ fi
 printf "source '$HOME/dotfiles/zsh/zshrc_manager.sh'" > ~/.zshrc
 printf "so $HOME/dotfiles/vim/vimrc.vim" > ~/.vimrc
 printf "source-file $HOME/dotfiles/tmux/tmux.conf" > ~/.tmux.conf
-
