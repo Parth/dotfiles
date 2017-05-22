2d1d8938536b6525c05dcca8a7cbbc6053a81e25
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2017-05-22 00:01:54 -0400

    tmux will now run as long as it's not already running

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -3,6 +3,8 @@ time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 # Run tmux if exists
 if command -v tmux>/dev/null; then
 	[ -z $TMUX ] && exec tmux
+else 
+	echo "tmux not installed. Run ./deploy to configure dependencies"
 fi
 
 echo "Updating configuration"
