f5dfdf080ea0e167dfc767dcd1833638cff8c7bd
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2017-08-20 11:02:29 -0400

    fixed git init bug

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -15,9 +15,10 @@ set_prompt() {
 	# Status Code
 	PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
 
+	# Git
 	if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
 		PS1+=', '
-		PS1+="%{$fg[blue]%}$(git rev-parse --abbrev-ref HEAD)%{$reset_color%}"
+		PS1+="%{$fg[blue]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
 		if [ $(git status --short | wc -l) -gt 0 ]; then 
 			PS1+="%{$fg[red]%}+$(git status --short | wc -l | awk '{$1=$1};1')%{$reset_color%}"
 		fi
