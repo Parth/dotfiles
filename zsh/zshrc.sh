05a10ebf82ade4d3563e6afb4879a06307d5e87b
Joshua Blakley <joshua.blakley@aa.com>
2019-03-12 16:06:48 -0700

    added nvm support

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -52,6 +52,7 @@ eval "`dircolors ~/dotfiles/zsh/dircolors.db`"
 autoload -U compinit
 
 plugins=(
+	nvm,
 	git,
 	docker
 )
@@ -87,3 +88,29 @@ fi
 source ~/dotfiles/zsh/prompt.sh
 export PATH=$PATH:$HOME/dotfiles/utils
 
+# Import nvm
+export NVM_DIR=~/dotfiles/utils/nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# NVM Use automatically
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
