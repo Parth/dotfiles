7103fbe7dd41ee49c159ff0fa253119424d6f9e3
Parth <parth@mehrotra.me>
2019-08-08 18:21:27 -0400

    fixed side-ffecting cd

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -8,17 +8,16 @@ else
 fi
 
 echo "Checking for updates."
-#(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
-{cd ~/dotfiles && git fetch -q} &> /dev/null
+({cd ~/dotfiles && git fetch -q} &> /dev/null)
  
 if [ $({cd ~/dotfiles} &> /dev/null && git rev-list HEAD...origin/master | wc -l) = 0 ]
 then
 	echo "Already up to date."
 else
 	echo "Updates Detected:"
-	{cd ~/dotfiles} &> /dev/null && git log ..@{u} --pretty=format:%Cred%aN:%Creset\ %s\ %Cgreen%cd
+	({cd ~/dotfiles} &> /dev/null && git log ..@{u} --pretty=format:%Cred%aN:%Creset\ %s\ %Cgreen%cd)
 	echo "Setting up..."
-	{cd ~/dotfiles} &> /dev/null && git pull -q && git submodule update --init --recursive
+	({cd ~/dotfiles} &> /dev/null && git pull -q && git submodule update --init --recursive)
 fi
 
 source ~/dotfiles/zsh/zshrc.sh
