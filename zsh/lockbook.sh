53e3a21fd38c81a4cd40b580b9cfea8402a3ba5c
Parth <parth@mehrotra.me>
2020-07-09 20:06:10 -0400

    edited alias

--- zsh/lockbook.sh
+++ zsh/lockbook.sh
@@ -1,6 +1,6 @@
 export LOCKBOOK_EDITOR="vim"
 
-alias lvim="lockbook list-docs | fzf | lockbook edit"
+alias lvim="lockbook list-docs | fzf | lockbook edit && lockbook sync"
 alias lcat="lockbook list | fzf | lockbook print"
 
 alias configure="vim ~/dotfiles/zsh/lockbook.sh"
