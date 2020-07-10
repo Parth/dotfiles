b0bc21121ddccb99407dfb14a2c2d84c338d4703
Parth <parth@mehrotra.me>
2020-07-09 20:01:41 -0400

    removed old aliases

--- zsh/lockbook.sh
+++ zsh/lockbook.sh
@@ -1,8 +1,6 @@
 export LOCKBOOK_EDITOR="vim"
 
-alias lls="lockbook list | fzf"
-alias lvim="lockbook list | fzf | lockbook edit"
+alias lvim="lockbook list-docs | fzf | lockbook edit"
 alias lcat="lockbook list | fzf | lockbook print"
-alias lrm="lockbook list | fzf | lockbook remove"
 
 alias configure="vim ~/dotfiles/zsh/lockbook.sh"
