bc55e79aa1c8542da022ad8a26b02f10d5a98648
Parth <parth@mehrotra.me>
2021-05-09 12:30:24 -0400

    fix ls

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -4,12 +4,13 @@
 
 
 # Custom cd
+source ~/dotfiles/zsh/plugins/fixls.zsh
+
 chpwd() ls
 
 
 autoload -U compinit && compinit
 
-source ~/dotfiles/zsh/plugins/fixls.zsh
 source ~/dotfiles/zsh/plugins/zsh-vi-mode.plugin.zsh
 source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
