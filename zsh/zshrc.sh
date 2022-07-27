d1755c1e7b295bdfce797dfaa7625163bfb67ff5
hai che <cheviethai123@gmail.com>
2022-07-27 02:40:49 +0000

    rm non-use command

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,83 +1,31 @@
 # Vars
-  HISTFILE=~/.zsh_history
-  SAVEHIST=1000
-  setopt inc_append_history # To save every command before it is executed
-  setopt share_history # setopt inc_append_history
+	HISTFILE=~/.zsh_history
+	SAVEHIST=10000
 
-git config --global push.default current
-
-mkdir -p /tmp/log
-
-
-source ~/dotfiles/zsh/plugins/fixls.zsh
-
-#Functions
-	# Loop a command and show the output in vim
-	loop() {
-		echo ":cq to quit\n" > /tmp/log/output 
-		fc -ln -1 > /tmp/log/program
-		while true; do
-			cat /tmp/log/program >> /tmp/log/output ;
-			$(cat /tmp/log/program) |& tee -a /tmp/log/output ;
-			echo '\n' >> /tmp/log/output
-			vim + /tmp/log/output || break;
-			rm -rf /tmp/log/output
-		done;
-	}
 
 # Custom cd
+source ~/dotfiles/zsh/plugins/fixls.zsh
+
 chpwd() ls
 
-# For vim mappings: 
-	stty -ixon
 
-# Completions
-# These are all the plugin options available: https://github.com/robbyrussell/oh-my-zsh/tree/291e96dcd034750fbe7473482508c08833b168e3/plugins
-#
-# Edit the array below, or relocate it to ~/.zshrc before anything is sourced
-# For help create an issue at github.com/parth/dotfiles
+autoload -U compinit && compinit
 
-autoload -U compinit
-
-plugins=(
-	docker
-)
-
-for plugin ($plugins); do
-    fpath=(~/dotfiles/zsh/plugins/oh-my-zsh/plugins/$plugin $fpath)
-done
-
-compinit
-
-source ~/dotfiles/aliases/.aliases
 source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/history.zsh
 source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/key-bindings.zsh
 source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/completion.zsh
-source ~/dotfiles/zsh/plugins/vi-mode.plugin.zsh
 source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/dotfiles/zsh/keybindings.sh
-
-# Fix for arrow-key searching
-# start typing + [Up-Arrow] - fuzzy find history forward
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-	autoload -U up-line-or-beginning-search
-	zle -N up-line-or-beginning-search
-	bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
-fi
-# start typing + [Down-Arrow] - fuzzy find history backward
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-	autoload -U down-line-or-beginning-search
-	zle -N down-line-or-beginning-search
-	bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
-fi
-
 source ~/dotfiles/zsh/prompt.sh
+
 # Add dir colors for terminal currently doesn't suppor well for mac
 if [[ "$OSTYPE" != "darwin*" ]]; then
 	eval $( dircolors -b ~/dotfiles/.dir_colors )
 fi
 
-export PATH=$PATH:$HOME/dotfiles/utils
+# Set default text editor to vim
+export VISUAL=vim
 
+# Utils
+export PATH=$PATH:$HOME/dotfiles/utils
 
