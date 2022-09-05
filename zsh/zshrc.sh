81e50b28c7ea713e08505996461284a7046db4fa
haiche <cheviethai123@gmail.com>
2022-09-05 11:45:31 +0700

    Update zshrc.sh
    rm dotenv

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -16,7 +16,6 @@ source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/history.zsh
 source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/functions.zsh
 source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/key-bindings.zsh
 source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/completion.zsh
-source ~/dotfiles/zsh/plugins/oh-my-zsh/plugins/dotenv/dotenv.plugin.zsh
 source ~/dotfiles/zsh/plugins/oh-my-zsh/plugins/web-search/web-search.plugin.zsh
 source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
