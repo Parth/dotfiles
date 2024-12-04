35c8d1326b5c3317aa30ac6915bcaf5d295f1197
parth <parth@mehrotra.me>
2024-12-04 00:16:34 -0500

    tty needed for server

--- nix/laptop.nix
+++ nix/laptop.nix
@@ -6,7 +6,8 @@
 
   networking.hostName = "parth-laptop-nix";
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  services.tlp.enable = true;
 }
