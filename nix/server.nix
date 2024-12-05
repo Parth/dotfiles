7ed2be82845687c2790dac39d76cc250369f1e4f
parth <parth@mehrotra.me>
2024-12-05 17:06:32 -0500

    restore CI?

--- nix/server.nix
+++ nix/server.nix
@@ -46,15 +46,23 @@
       PrivateDevices = false;
     };
     extraEnvironment = {
-      PKG_CONFIG_PATH = lib.makeSearchPath "pkgconfig" [
-        pkgs.gtk3.dev
-        pkgs.glib.dev
-        pkgs.gdk-pixbuf.dev
-        pkgs.pango.dev
-        pkgs.cairo.dev
-        pkgs.gobject-introspection.dev
-      ];
-      # PKG_CONFIG_PATH = "${pkgs.gtk3.dev}/lib/pkgconfig:${pkgs.glib.dev}/lib/pkgconfig:${pkgs.gobject-introspection.dev}/lib/pkgconfig:${pkgs.gdk-pixbuf.dev}/lib/pkgconfig:${pkgs.atk.dev}/lib/pkgconfig";
+      # PKG_CONFIG_PATH = lib.makeSearchPath "pkgconfig" [
+      #   pkgs.gtk3.dev
+      #   pkgs.glib.dev
+      #   pkgs.gdk-pixbuf.dev
+      #   pkgs.pango.dev
+      #   pkgs.cairo.dev
+      #   pkgs.gobject-introspection.dev
+      # ];
+      PKG_CONFIG_PATH = ''
+        ${pkgs.gtk3.dev}/lib/pkgconfig:
+        ${pkgs.glib.dev}/lib/pkgconfig:
+        ${pkgs.gobject-introspection.dev}/lib/pkgconfig:
+        ${pkgs.gdk-pixbuf.dev}/lib/pkgconfig:
+        ${pkgs.atk.dev}/lib/pkgconfig
+        ${pkgs.cairo.dev}/lib/pkgconfig
+        ${pkgs.pango.dev}/lib/pkgconfig
+      '';
     };
   };
 
