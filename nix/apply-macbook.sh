699af4cf095193379b8bbc753e8445e96ca1e6ee
parth <parth@mehrotra.me>
2025-09-16 15:20:37 -0400

    cleanup mac flake

--- nix/apply-macbook.sh
+++ nix/apply-macbook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf /etc/nix-darwin/flake.nix && cp $HOME/dotfiles/nix/macbook.nix /etc/nix-darwin/flake.nix && nix run nix-darwin/master#darwin-rebuild -- switch --impure
+sudo darwin-rebuild switch --impure --flake .#default
